e8d36d32ade5553d1bd767c085794d55401eab49
julianodibai <julianodibai2008@gmail.com>
2023-01-15 14:37:36 -0300

    finalizando o app de previsão do tempo

--- PrevisaoTempo/app.js
+++ PrevisaoTempo/app.js
@@ -1,22 +1,39 @@
-const express = require("express");
 const https = require("https");
+const express = require("express");
+const bodyParser = require('body-parser')
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=brasilia&units=metric&appid=4786c0cfb939ae9f792cc4d0af3f348d";
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-    https.get(url, (response)=>{
-        response.on("data", (data)=>{
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+});
+
+app.post("/", (req, res) => {
+
+    const city = req.body.cityName
+    const appId = process.env.OpenWeatherMapAppId;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + appId;
+
+    https.get(url, (response) => {
+        if (response.statusCode == `404`) {
+            res.send("<h1>Cidade não encontrada</h1>")
+        }
+
+        response.on("data", (data) => {
             const tempoDados = JSON.parse(data);
             const temperatura = tempoDados.main.temp;
             const descricao = tempoDados.weather[0].description;
             const icon = tempoDados.weather[0].icon;
-            const imageUrl = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-            
-            res.write("<h1>Temperatura de Brasilia:"+ temperatura + " graus Celcius.</h1>");
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+            res.write("<h1>Temperatura de " + tempoDados.name + ":" + temperatura + " graus Celcius.</h1>");
             res.write("<p>Description: " + descricao + "</p>");
-            res.write("<img src="+imageUrl+">");
+            res.write("<img src=" + imageUrl + ">");
             res.send();
 
             console.log(tempoDados);
@@ -24,6 +41,7 @@ app.get("/", (req, res)=>{
     });
 });
 
-app.listen(3000, ()=>{
+
+app.listen(3000, () => {
     console.log("http://localhost:3000/")
 });
